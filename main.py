5a69501de697427460e6eba1b9f85c299e8c57f3
Jgmaza <49261949+Jgmaza@users.noreply.github.com>
2022-03-14 05:24:14 -0500

    Solo falta una tabla

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from asyncio.windows_events import NULL
+from pickle import BININT
 from models import *
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask,Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, request
 
 
 app = Flask(__name__)
@@ -11,10 +13,57 @@ db = SQLAlchemy(app)
 m = Blueprint('main', __name__)
 
 
-@m.route('/')
+@m.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        print("a")
+        try:
+            eloc = int(request.form['e-loc']) - 1
+            evis = int(request.form['e-vis']) - 1
+            loc = int(request.form['loc'])
+            vis = int(request.form['vis'])
+            c = Partido(equipo_local=eloc,
+                        equipo_visitante=evis,
+                        goles_local=loc,
+                        goles_visitante=vis)
+            db.session.merge(c)
+            db.session.commit()
+        except:
+            try:
+                ciudad = int(request.form['ciudad']) - 1
+                division = int(request.form['division']) - 1
+                nombre = (request.form['nombre'])
+                num = int(request.form['num_jugadores'])
+                c = Equipo(nombre=nombre,
+                           num_jugadores=num,
+                           division=division,
+                           ciudad=ciudad)
+                db.session.merge(c)
+                db.session.commit()
+            except:
+                try:
+                    equipo = int(request.form['equipo']) - 1
+                    e = Equipo.query.filter(Equipo.id == equipo).one()
+                    e.num_jugadores += 1 
+                    db.session.merge(e)
+                    db.session.commit()
+                    nombre = (request.form['nombre'])
+                    edad = int(request.form['edad'])
+                    sueldo = int(request.form['sueldo'])
+                    tr = int(request.form['tr'])
+                    ta = int(request.form['ta'])
+                    goles = int(request.form['goles'])
+                    pj = int(request.form['pj'])
+                    c = Jugador(nombre=nombre,
+                                id_equipo=equipo,
+                                edad=edad,
+                                sueldo=sueldo,
+                                tr=tr, ta=ta,
+                                goles=goles, pj=pj)
+                    db.session.merge(c)
+                    db.session.commit()
+                except:
+                    print("F")
+
     jugadores1 = Jugador.query.all()
     equipos1 = Equipo.query.all()
     equipos = []
@@ -29,7 +78,8 @@ def index():
                           "edad": e.edad, "sueldo": e.sueldo, "tr": e.tr, "ta": e.ta,
                           "goles": e.goles, "pj": e.pj})
     ciudades = Ciudad.query.all()
-    return render_template('index.html', ciudades=ciudades, equipos=equipos, jugadores=jugadores)
+    divisiones = Division.query.all()
+    return render_template('index.html', divisiones=divisiones, ciudades=ciudades, equipos=equipos, jugadores=jugadores)
 
 
 app.register_blueprint(m)
