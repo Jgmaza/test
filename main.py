342a29b22646d618bc73b624321b86a0aefeb358
Jgmaza <49261949+Jgmaza@users.noreply.github.com>
2022-03-14 03:23:46 -0500

    Avance

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from models import *
 from flask_sqlalchemy import SQLAlchemy
-from common.routes import main
+from flask import Flask,Blueprint, render_template, request
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-from models import *
-app.register_blueprint(main)
+
+m = Blueprint('main', __name__)
+
+
+@m.route('/')
+def index():
+    if request.method == 'POST':
+        print("a")
+    jugadores1 = Jugador.query.all()
+    equipos1 = Equipo.query.all()
+    equipos = []
+    jugadores = []
+    for e in equipos1:
+        equipos.append({"id": e.id, "nombre": e.nombre, "num_jugadores": e.num_jugadores,
+                        "ciudad": Ciudad.query.filter(Ciudad.id == e.ciudad).one().nombre,
+                        "division": Division.query.filter(Division.id == e.division).one().nombre})
+    for e in jugadores1:
+        jugadores.append({"id": e.id, "nombre": e.nombre,
+                          "equipo": Equipo.query.filter(Equipo.id == e.id_equipo).one().nombre,
+                          "edad": e.edad, "sueldo": e.sueldo, "tr": e.tr, "ta": e.ta,
+                          "goles": e.goles, "pj": e.pj})
+    ciudades = Ciudad.query.all()
+    return render_template('index.html', ciudades=ciudades, equipos=equipos, jugadores=jugadores)
+
+
+app.register_blueprint(m)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,47 @@
 from main import db
 
-class Usuario(db.Model):
-    
+
+class Division(db.Model):
+    __tablename__ = 'division'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
+    nombre = db.Column(db.String(50), nullable=False)
+
+
+class Ciudad(db.Model):
+    __tablename__ = 'ciudad'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+
+class Equipo(db.Model):
+
+    __tablename__ = 'equipo'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    num_jugadores = db.Column(db.Integer, nullable=False)
+    ciudad = db.Column(db.Integer, db.ForeignKey('ciudad.id'))
+    division = db.Column(db.Integer, db.ForeignKey('division.id'))
+
+
+class Jugador(db.Model):
+
+    __tablename__ = 'jugador'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
+    sueldo = db.Column(db.BigInteger, nullable=False)
+    tr = db.Column(db.Integer, nullable=False)
+    ta = db.Column(db.Integer, nullable=False)
+    goles = db.Column(db.Integer, nullable=False)
+    pj = db.Column(db.Integer, nullable=False)
+    id_equipo = db.Column(db.Integer, db.ForeignKey('equipo.id'))
+
+
+class Partido(db.Model):
+
+    __tablename__ = 'partido'
+    id = db.Column(db.Integer, primary_key=True)
+    equipo_local = db.Column(db.Integer, db.ForeignKey('equipo.id'))
+    equipo_visitante = db.Column(db.Integer, db.ForeignKey('equipo.id'))
+    goles_local = db.Column(db.Integer, nullable=False)
+    goles_visitante = db.Column(db.Integer, nullable=False)
